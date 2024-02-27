56c01ab68b8dc52be5a5a263e03b1d5133f36228
nikosoo <130840899+nikosoo@users.noreply.github.com>
2024-02-27 22:44:11 +0200

    updated css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/header/header.component.html",
-    "./src/app/footer/footer.component.html",
-    "./src/app/home/home.component.html",
-    "./src/app/home/product-card/product-card.component.html",
-    "./src/app/products/products.component.html",
-    "./src/app/contact/contact.component.html",
-    "./src/app/products/products1/products1.component.html",
-    "./src/app/products/products-men/products-men.component.html",
-    "./src/app/products/products-view/products-view.component.html",
-    "./src/app/cart/cart.component.html",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
